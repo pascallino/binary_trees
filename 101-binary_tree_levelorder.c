59f04ac22014279c844462cda91b876aa104f4f2
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-28 06:40:09 -0700

    101-

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+
+/**
+ * height4levelorder - height of a tree
+ * @root: nodes
+ * Return: height
+ */
+
+size_t height4levelorder(const binary_tree_t *root)
+{
+	size_t left = 0, right = 0;
+
+	if (root)
+	{
+
+		left =   root->left ? 1 + height4levelorder(root->left) : 0;
+		right =  root->right ? 1 + height4levelorder(root->right) : 0;
+	}
+	return ((left > right ? left : right));
+}
+
+/**
+ * binary_levelorder_helper - helper function to move
+ * from level to level for each node
+ * @tree: node
+ * @func: print function
+ * @level: level of the node
+ */
+
+void binary_levelorder_helper(const binary_tree_t *tree,
+		void (*func)(int), size_t level)
+{
+
+	if (level == 1)
+	{
+		func(tree->n);
+	}
+	else
+	{
+		binary_levelorder_helper(tree->left, func, level - 1);
+		binary_levelorder_helper(tree->right, func, level - 1);
+	}
+}
+/**
+ * binary_tree_levelorder - print from level to level for each node
+ * @tree: node
+ * @func: print function
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	int height = height4levelorder(tree) + 1;
+	int level = 1;
+
+	while (level <= height)
+	{
+		binary_levelorder_helper(tree, func, level);
+		level++;
+
+	}
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_root(const binary_tree_t *node);
@@ -48,5 +49,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 int isDescendant(const binary_tree_t *root, const binary_tree_t *node);
-
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+size_t height4levelorder(const binary_tree_t *root);
+void binary_levelorder_helper(const binary_tree_t *tree,
+		void (*func)(int), size_t level);
 #endif
