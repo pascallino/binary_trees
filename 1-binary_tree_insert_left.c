86ae56fcc912f4b1cd59368f00b29b88818252ff
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-28 22:15:14 -0700

    binary_trees.h

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
+	if (!parent)
+		return (NULL);
+
 	newnode = binary_tree_node(parent, value);
 	if (newnode == NULL)
 	{
