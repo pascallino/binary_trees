71c142640388cb704b81b262f9709e284860fec6
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-29 06:55:03 -0700

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,62 +1,28 @@
 #include "binary_trees.h"
 
-
-/* Helper function to check if a node is in the subtree*/
-/* from the parent of the */
-/* first node recursively*/
 /**
- * isDescendant - check if  the second node is a decendant of the first nodes
- * parent
- * @root: first node root(s)
- * @node: second node to scan from
- * Return: 1 or 0
- */
-int isDescendant(const binary_tree_t *root, const binary_tree_t *node)
-{
-	if (root == NULL)
-	{
-		return (0);
-	}
-
-	if (root == node)
-	{
-		return (1);
-	}
-
-	return (isDescendant(root->left, node) || isDescendant(root->right, node));
-}
-
-/**
- * binary_trees_ancestor - find the anscestor of two nodes
- * @first: first node root(s)
- * @second: second node to scan from
- * Return: anscestor
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: a pointer to the first node to find the ancestor
+ * @second: a pointer to the second node to find the ancestor
+ *
+ * Return: pointer to the ancestor node
+ *         NULL if there is no ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	const binary_tree_t *current;
-
 	if (!first || !second)
 		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
 
-	current = first;
-	while (current != NULL)
+	const binary_tree_t *temp_first = first;
+	const binary_tree_t *temp_second = second;
+
+	while (temp_first != temp_second)
 	{
-		if (isDescendant(current->left, second))
-		{
-			return ((binary_tree_t *)current);
-		} else if (isDescendant(current->right, second))
-		{
-			return ((binary_tree_t *)current);
-		}
-
-		current = current->parent;
+		temp_first = (temp_first) ? temp_first->parent : second;
+		temp_second = (temp_second) ? temp_second->parent : first;
 	}
 
-	/*return (NULL);*/ /* No common ancestor found*/
-	return ((binary_tree_t *)first);
+	return ((binary_tree_t *)temp_first);
 }
 
