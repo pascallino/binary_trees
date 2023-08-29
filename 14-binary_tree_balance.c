afd0c4f2fdf8afea489c93281dd1aca57854bcd4
pascallino <luvpascal.ojukwu@yahoo.com>
2023-08-28 22:45:04 -0700

    2-binary_tree_insert_right.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,6 +28,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	int left = 0, right = 0;
 
+	if (!tree)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
